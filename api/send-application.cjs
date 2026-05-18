b9eb9fa3e1a1df0e0bd9966ffb9d81f155d883de
valerie-ekeigwe <valerie.ekeigwe@gmail.com>
2026-05-18 19:43:17 -0400

    use esm syntax in cjs file

--- api/send-application.cjs
+++ api/send-application.cjs
@@ -1,4 +1,4 @@
-const handler = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
@@ -62,5 +62,3 @@ const handler = async (req, res) => {
     return res.status(500).json({ error: 'Unexpected server error', detail: String(e) })
   }
 }
-
-module.exports = handler
